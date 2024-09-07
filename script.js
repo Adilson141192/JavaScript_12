3025988c49c985ecc98f1f3445b6226e592b8454
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-09-07 11:33:32 -0300

    090724 aula0301

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function adicionarItem(evento) {
 
     const itemDaLista = document.createElement("li");
     const containerItemLista = document.createElement("div");
-    containerItemLista.classlist.add("lista-item-container");
+    containerItemLista.classList.add("lista-item-container");
 
     const containerNomeDoItem = document.createElement("div");
 
@@ -19,23 +19,23 @@ function adicionarItem(evento) {
     const checkboxInput = document.createElement("input");
     checkboxInput.type = "checkbox";
     checkboxInput.classList.add("input-checkbox");
-    checkboxInput.ad = "checkbox-" + contador++;
+    checkboxInput.id = "checkbox-" + contador++;
 
     const checkboxLabel = document.createElement("label");
     checkboxLabel.setAttribute("for", checkboxInput.id);
 
     const checkboxCustomizado = document.createElement("div");
-    checkboxCustomizado.classList.add("checkbox-customizad");
+    checkboxCustomizado.classList.add("checkbox-customizado");
 
     checkboxLabel.appendChild(checkboxInput);
     checkboxLabel.appendChild(checkboxCustomizado);
 
     containerCheckbox.appendChild(checkboxLabel);
-    containerNomeDoItem.appendChild(containerCheckbox);
+    containerNomeDoItem.appendChild(containerCheckbox)
 
     const nomeDoItem = document.createElement("p");
-    nomeDoItem.innerHTML = item.value;
-    containerNomeDoItem.appendChild(nomeDoItem);
+    nomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(nomeDoItem)
 
     const containerBotoes = document.createElement("div");
     const botaoRemover = document.createElement("button");
@@ -44,12 +44,22 @@ function adicionarItem(evento) {
     const imagemRemover = document.createElement("img");
     imagemRemover.src = "img/delete.svg";
     imagemRemover.alt = "Remover";
-     
+
     botaoRemover.appendChild(imagemRemover);
     containerBotoes.appendChild(botaoRemover);
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("item-lista-button")
+
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+
+    botaoEditar.appendChild(imagemEditar);
+    containerBotoes.appendChild(botaoEditar);
 
     containerItemLista.appendChild(containerNomeDoItem);
     containerItemLista.appendChild(containerBotoes);
     itemDaLista.appendChild(containerItemLista);
-    listaDeCompras.appendChild(itemDaLista);
+    listaDeCompras.appendChild(itemDaLista)
 }
